dc58448dba5969ee6188909c0899251349020355
mailsonsoc <albuquerque.soc@gmail.com>
2023-10-30 12:10:37 -0300

    Segundo Commit

--- Server_Mantenedor/main.go
+++ Server_Mantenedor/main.go
@@ -1,86 +1,78 @@
 package main
 
 import (
-	"fmt"
 	"html/template"
 	"net/http"
-	"strings"
 
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
 
-type Todo struct {
+type Produto struct {
 	gorm.Model
-	ID    uint
-	Title string
-	Done  bool
+	ID          uint
+	NomeProduto string
+	ValorCompra float64
+	ValorVenda  float64
 }
-type TodoPageData struct {
+
+type ProdutoPageData struct {
 	PageTitle string
-	Todos     []Todo
+	Produtos  []Produto
 }
 
 func main() {
-	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	db, err := gorm.Open(sqlite.Open("product.db"), &gorm.Config{})
 	if err != nil {
-		panic("failed to connect database")
+		panic("Failed to connect database")
 	}
-	db.AutoMigrate(&Todo{})
+	db.AutoMigrate(&Produto{})
 
 	var count int64
-	db.Model(&Todo{}).Count(&count)
+	db.Model(&Produto{}).Count(&count)
 
 	if count == 0 {
-		// A tabela está vazia, então insira dados iniciais
-		initialTodos := []Todo{
-			{Title: "Tarefa 1", Done: false},
-			{Title: "Tarefa 2", Done: false},
-			{Title: "Tarefa 3", Done: true},
+		// Se a tabela estiver vazia, insira alguns produtos iniciais
+		initialProdutos := []Produto{
+			{NomeProduto: "Café Espresso", ValorCompra: 2.50, ValorVenda: 3.50},
+			{NomeProduto: "Cappuccino", ValorCompra: 3.00, ValorVenda: 4.50},
+			{NomeProduto: "Café Americano", ValorCompra: 2.00, ValorVenda: 3.00},
+			{NomeProduto: "Latte Macchiato", ValorCompra: 3.50, ValorVenda: 4.50},
+			{NomeProduto: "Mocha", ValorCompra: 3.50, ValorVenda: 4.75},
+			{NomeProduto: "Chá Verde", ValorCompra: 2.00, ValorVenda: 3.25},
+			{NomeProduto: "Chá de Camomila", ValorCompra: 2.00, ValorVenda: 3.25},
+			{NomeProduto: "Café Descafeinado", ValorCompra: 2.75, ValorVenda: 4.00},
+			{NomeProduto: "Café com Leite", ValorCompra: 3.00, ValorVenda: 4.25},
+			{NomeProduto: "Chocolatte", ValorCompra: 3.50, ValorVenda: 4.75},
+			{NomeProduto: "Croissant", ValorCompra: 2.50, ValorVenda: 3.75},
+			{NomeProduto: "Muffin de Blueberry", ValorCompra: 2.75, ValorVenda: 4.00},
+			{NomeProduto: "Sanduíche de Presunto e Queijo", ValorCompra: 4.50, ValorVenda: 6.00},
+			{NomeProduto: "Bolo de Chocolate", ValorCompra: 3.25, ValorVenda: 4.75},
+			{NomeProduto: "Bolo de Cenoura", ValorCompra: 3.25, ValorVenda: 4.75},
+			{NomeProduto: "Torta de Maçã", ValorCompra: 3.50, ValorVenda: 5.25},
+			{NomeProduto: "Donuts", ValorCompra: 1.50, ValorVenda: 2.75},
+			{NomeProduto: "Água Mineral", ValorCompra: 1.00, ValorVenda: 1.75},
+			{NomeProduto: "Suco de Laranja", ValorCompra: 2.50, ValorVenda: 4.00},
+			{NomeProduto: "Smoothie de Frutas", ValorCompra: 3.50, ValorVenda: 5.25},
 		}
 
-		for _, todo := range initialTodos {
-			db.Create(&todo)
+		for _, produto := range initialProdutos {
+			db.Create(&produto)
 		}
 	}
 
 	tmpl := template.Must(template.ParseFiles("template/index.html"))
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		if r.Method == "POST" {
-			// Call ParseForm() to parse the raw query and update r.PostForm and r.Form.
-			if err := r.ParseForm(); err != nil {
-				fmt.Fprintf(w, "ParseForm() err: %v", err)
-				return
-			}
-			todo := r.FormValue("todo")
-			db.Create(&Todo{Title: todo, Done: false})
-		}
-		//Request not POST
-		var todos []Todo
-		db.Find(&todos)
-		data := TodoPageData{
-			PageTitle: "My TODO list",
-			Todos:     todos,
+		// Não é mais uma solicitação POST, então removemos o código relacionado à adição de produtos aqui
+		var produtos []Produto
+		db.Find(&produtos)
+		data := ProdutoPageData{
+			PageTitle: "Coffee Shop - Manutenção de Estoque",
+			Produtos:  produtos,
 		}
 		tmpl.Execute(w, data)
 	})
 
-	http.HandleFunc("/done/", func(w http.ResponseWriter, r *http.Request) {
-		id := strings.TrimPrefix(r.URL.Path, "/done/")
-		var todo Todo
-		db.First(&todo, id)
-		todo.Done = true
-		db.Save(&todo)
-		http.Redirect(w, r, "/", http.StatusSeeOther)
-	})
-
-	http.HandleFunc("/delete/", func(w http.ResponseWriter, r *http.Request) {
-		id := strings.TrimPrefix(r.URL.Path, "/delete/")
-		db.Delete(&Todo{}, id)
-		http.Redirect(w, r, "/", http.StatusSeeOther)
-	})
-
 	http.ListenAndServe(":8080", nil)
-
 }
