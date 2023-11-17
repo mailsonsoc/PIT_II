e0b7d2d416ce9836bb4a40f3e6e3ec39414a1b38
mailsonsoc <albuquerque.soc@gmail.com>
2023-11-17 12:22:08 -0300

    primeiro commit 17-11

--- Server_Usuario/main.go
+++ Server_Usuario/main.go
@@ -1,65 +1,48 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
-
-	"github.com/gorilla/mux"
 )
 
 func main() {
-	r := mux.NewRouter()
+	// Configuração do servidor de arquivos estáticos
+	fs := http.FileServer(http.Dir("template"))
+	http.Handle("/", fs)
 
-	// Rota para a página inicial
-	r.HandleFunc("/", HomeHandler)
+	// Roteamento para diferentes endpoints
+	http.HandleFunc("/pagina_inicial", paginaInicialHandler)
+	http.HandleFunc("/catalogo", catalogoHandler)
+	http.HandleFunc("/sobre_nos", sobreNosHandler)
+	http.HandleFunc("/fale_conosco", faleConoscoHandler)
+	http.HandleFunc("/carrinho", carrinhoHandler)
 
-	// Rota para a página inicial
-	r.HandleFunc("/pagina_inicial", HomeHandler)
-
-	// Rota para a página catalogo.html
-	r.HandleFunc("/catalogo", CatalogoHandler)
-
-	// Rota para a página sobre_nos.html
-	r.HandleFunc("/sobre_nos", SobreNosHandler)
-
-	// Rota para a página fale_conosco.html
-	r.HandleFunc("/fale_conosco", FaleConoscoHandler)
-
-	// Rota para a página carrinho.html
-	r.HandleFunc("/carrinho", CarrinhoHandler)
-
-	// Manipulador para servir os arquivos estáticos do diretório /template/
-	r.PathPrefix("/template/").Handler(http.StripPrefix("/template/", http.FileServer(http.Dir("./template/"))))
-
-	// Configuração do servidor para escutar na porta 8081
+	// Definindo o endereço e porta do servidor
 	port := ":8081"
-	fmt.Printf("Servidor rodando em http://localhost%s\n", port)
-	if err := http.ListenAndServe(port, r); err != nil {
-		fmt.Println(err)
+
+	// Iniciando o servidor
+	err := http.ListenAndServe(port, nil)
+	if err != nil {
+		panic(err)
 	}
 }
 
-// Handler para a página inicial
-func HomeHandler(w http.ResponseWriter, r *http.Request) {
-	http.Redirect(w, r, "./template/", http.StatusSeeOther)
+// Handlers para cada endpoint
+func paginaInicialHandler(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "template/index.html")
 }
 
-// Handler para a página catalogo.html
-func CatalogoHandler(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, "./template/catalogo.html")
+func catalogoHandler(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "template/catalogo.html")
 }
 
-// Handler para a página sobre_nos.html
-func SobreNosHandler(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, "./template/sobre_nos.html")
+func sobreNosHandler(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "template/sobre_nos.html")
 }
 
-// Handler para a página fale_conosco.html
-func FaleConoscoHandler(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, "./template/fale_conosco.html")
+func faleConoscoHandler(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "template/fale_conosco.html")
 }
 
-// Handler para a página carrinho.html
-func CarrinhoHandler(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, "./template/carrinho.html")
+func carrinhoHandler(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "template/carrinho.html")
 }
