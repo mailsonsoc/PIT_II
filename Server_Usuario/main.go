811fe9c0e8d7f8c2078d564bdbea86b4edab846a
mailsonsoc <albuquerque.soc@gmail.com>
2023-11-17 14:24:01 -0300

    segundo commit 17-11

--- Server_Usuario/main.go
+++ Server_Usuario/main.go
@@ -1,10 +1,30 @@
 package main
 
 import (
+	"fmt"
+	"html/template"
 	"net/http"
+
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 )
 
+type Produto struct {
+	gorm.Model
+	ID          uint
+	NomeProduto string
+	ValorCompra float64
+	ValorVenda  float64
+}
+
+type ProdutoPageData struct {
+	PageTitle string
+	Produtos  []Produto
+	Produto   Produto
+}
+
 func main() {
+
 	// Configuração do servidor de arquivos estáticos
 	fs := http.FileServer(http.Dir("template"))
 	http.Handle("/", fs)
@@ -20,9 +40,9 @@ func main() {
 	port := ":8081"
 
 	// Iniciando o servidor
-	err := http.ListenAndServe(port, nil)
-	if err != nil {
-		panic(err)
+	conn := http.ListenAndServe(port, nil)
+	if conn != nil {
+		panic(conn)
 	}
 }
 
@@ -32,7 +52,36 @@ func paginaInicialHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func catalogoHandler(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, "template/catalogo.html")
+	// Abre o banco de dados no diretório atual
+	db, err := gorm.Open(sqlite.Open("C:/Users/albuq/go/src/PIT_II/Server_Usuario/product.db"), &gorm.Config{})
+	if err != nil {
+		panic("Failed to connect database")
+	}
+
+	// Migrate (cria) a tabela Produto se ela ainda não existir
+	if err := db.AutoMigrate(&Produto{}); err != nil {
+		http.Error(w, fmt.Sprintf("Failed to migrate database: %s", err.Error()), http.StatusInternalServerError)
+		return
+	}
+
+	// Realiza uma busca por todos os produtos na tabela
+	var produtos []Produto
+	if err := db.Find(&produtos).Error; err != nil {
+		http.Error(w, fmt.Sprintf("Failed to retrieve data from database: %s", err.Error()), http.StatusInternalServerError)
+		return
+	}
+
+	// Carrega os dados na página HTML
+	tmpl := template.Must(template.ParseFiles("template/catalogo.html"))
+	data := ProdutoPageData{
+		PageTitle: "Coffee Shop - Catalogo",
+		Produtos:  produtos,
+	}
+
+	if err := tmpl.Execute(w, data); err != nil {
+		http.Error(w, fmt.Sprintf("Failed to execute template: %s", err.Error()), http.StatusInternalServerError)
+		return
+	}
 }
 
 func sobreNosHandler(w http.ResponseWriter, r *http.Request) {
